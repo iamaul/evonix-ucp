35683bee4556aaa5cb908f3322e39a2922b4ecdb
Agung Maulana <iamaul@hotmail.com>
2020-05-12 20:27:25 +0700

    updated latest changes

--- src/components/pages/users/applications/ScenarioItem.jsx
+++ src/components/pages/users/applications/ScenarioItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
