916c7df9e7d220a2b2e22f3ac20cd3a8feea601d
Agung Maulana <iamaul@hotmail.com>
2020-05-12 20:04:59 +0700

    updated latest changes

--- src/components/pages/users/applications/Scenario.jsx
+++ src/components/pages/users/applications/Scenario.jsx
@@ -1,41 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {
     Grid,
     Header,
     Segment,
-    Image,
-    Label,
-    Form
+    Image
 } from 'semantic-ui-react';
 
-import { quizResult } from '../../../actions/quiz';
+import ScenarioItem from './ScenarioItem';
 
 import Loader from '../../../layouts/loader/Loader';
 
-const Scenario = ({ 
-    quizResult, 
-    auth: { user }, 
-    quiz: { scenarios, score, setLoading }, 
-    history 
-}) => {
-    const [answer, setAnswer] = useState('');
-
-    // const { id, title, image, question } = scenarios;
-    console.log(scenarios);
-    const userId = user.id;
-    const quizId = scenarios.id;
-    const data = { userId, quizId, score, answer };
-
-    const onSubmit = e => {
-        e.preventDefault();
-        quizResult(data, history);
-    }
-
-    const onHandleChange = e => setAnswer(e.target.value);
-
+const Scenario = ({ auth: { user }, quiz: { scenarios, score, setLoading } }) => {
     return (
         <>
             <Grid textAlign="center" style={{ height: "60vh" }} verticalAlign="middle">
@@ -47,36 +24,11 @@ const Scenario = ({
                         <Header as="h3" textAlign="center">
                             Step 2: Scenario
                         </Header>
-                        { setLoading ? (<Loader isLoading={setLoading} />) : (<>
-                                <Label ribbon size="small">{scenarios.title}</Label>
-                                <Image src={scenarios.image} bordered />
-                                <small>
-                                    <i>
-                                        <p style={{ textAlign: "center" }}>
-                                            Gambar di atas merupakan ilustrasi dari scenario yang dibuat.
-                                        </p>
-                                    </i>
-                                </small>
-                                <br />
-                                <p style={{ textAlign: "justify" }}>{scenarios.question}</p>
-                                <Form size="small" onSubmit={onSubmit}>
-                                    <Form.Field>
-                                        <textarea 
-                                            name="answer"
-                                            placeholder="Answer here ..."
-                                            value={answer}
-                                            onChange={onHandleChange} 
-                                            rows="5" 
-                                            col="5" 
-                                        />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Form.Button color="red" size="tiny" content="Submit" />
-                                    </Form.Field>
-                                </Form>
-                            </>
+                        { setLoading ? (<Loader isLoading={setLoading} />) : (
+                            scenarios.map((scenario) => (
+                                <ScenarioItem item={scenario} userId={user.id} score={score} />
                             )
-                        }
+                        ))}
                     </Segment>
                 </Grid.Column>
             </Grid>
@@ -85,7 +37,6 @@ const Scenario = ({
 }
 
 Scenario.propTypes = {
-    quizResult: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     quiz: PropTypes.object.isRequired
 }
@@ -95,4 +46,4 @@ const mapStateToProps = state => ({
     quiz: state.quiz
 });
 
-export default connect(mapStateToProps, { quizResult })(withRouter(Scenario));
+export default connect(mapStateToProps)(Scenario);
---
+++ src/components/pages/users/applications/ScenarioItem.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {
+    Label,
+    Form,
+    Image
+} from 'semantic-ui-react';
+
+import { quizResult } from '../../../actions/quiz';
+
+const ScenarioItem = ({ quizResult, item, userId, score, history }) => {
+    const [answer, setAnswer] = useState('');    
+
+    const { id, title, question, image } = item;
+
+    const data = { userId, id, score, answer };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        quizResult(data, history);
+    }
+
+    const onHandleChange = e => setAnswer(e.target.value);
+
+    return (
+        <>
+            <Label ribbon size="small">{title}</Label>
+            <Image src={image} bordered />
+            <small>
+                <i>
+                    <p style={{ textAlign: "center" }}>
+                        Gambar di atas merupakan ilustrasi dari scenario yang dibuat.
+                    </p>
+                </i>
+            </small>
+            <br />
+            <p style={{ textAlign: "justify" }}>{question}</p>
+            <Form size="small" onSubmit={onSubmit}>
+                <Form.Field>
+                    <textarea 
+                        name="answer"
+                        placeholder="Answer here ..."
+                        value={answer}
+                        onChange={onHandleChange} 
+                        rows="5" 
+                        col="5" 
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <Form.Button color="red" size="tiny" content="Submit" />
+                </Form.Field>
+            </Form>
+        </>
+    )
+}
+
+ScenarioItem.propTypes = {
+    quizResult: PropTypes.func.isRequired
+}
+
+export default connect(null, { quizResult })(withRouter(ScenarioItem));
