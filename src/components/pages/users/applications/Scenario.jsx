fd59e7f9ffbf9c9c5d6bdc92caf3b294ebb210a2
Agung Maulana <iamaul@hotmail.com>
2020-05-12 17:46:51 +0700

    updated latest changes

--- src/components/pages/users/applications/Scenario.jsx
+++ src/components/pages/users/applications/Scenario.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -6,7 +6,9 @@ import {
     Grid,
     Header,
     Segment,
-    Image
+    Image,
+    Label,
+    Form
 } from 'semantic-ui-react';
 
 import { loadQuizScenario, quizResult } from '../../../actions/quiz';
