56ca679d6ce71ff942d5eb815fbd102e922c182d
Agung Maulana <iamaul@hotmail.com>
2020-05-12 17:17:52 +0700

    updated latest changes

--- src/components/pages/users/applications/Scenario.jsx
+++ src/components/pages/users/applications/Scenario.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {
     Grid,
@@ -8,15 +9,33 @@ import {
     Image
 } from 'semantic-ui-react';
 
-import { loadQuizScenario } from '../../../actions/quiz';
+import { loadQuizScenario, quizResult } from '../../../actions/quiz';
 
 import Loader from '../../../layouts/loader/Loader';
-import ScenarioItem from './ScenarioItem';
 
-const Scenario = ({ loadQuizScenario, quiz: { scenarios, setLoading } }) => {
+const Scenario = ({ 
+    loadQuizScenario, 
+    quizResult, 
+    auth: { user }, 
+    quiz: { scenarios, score, setLoading }, 
+    history 
+}) => {
     useEffect(() => {
         loadQuizScenario();
     }, [loadQuizScenario])
+
+    const [answer, setAnswer] = useState('');
+
+    const { id, title, image, question } = scenarios;
+    const userId = user.id;
+    const data = { userId, id, score, answer };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        quizResult(data, history);
+    }
+
+    const onHandleChange = e => setAnswer(e.target.value);
 
     return (
         <>
@@ -29,8 +48,34 @@ const Scenario = ({ loadQuizScenario, quiz: { scenarios, setLoading } }) => {
                         <Header as="h3" textAlign="center">
                             Step 2: Scenario
                         </Header>
-                        { setLoading ? (<Loader isLoading={setLoading} />) : (
-                                <ScenarioItem item={scenarios} />
+                        { setLoading ? (<Loader isLoading={setLoading} />) : (<>
+                                <Label ribbon size="small">{title}</Label>
+                                <Image src={image} bordered />
+                                <small>
+                                    <i>
+                                        <p style={{ textAlign: "center" }}>
+                                            Gambar di atas merupakan ilustrasi dari scenario yang dibuat.
+                                        </p>
+                                    </i>
+                                </small>
+                                <br />
+                                <p style={{ textAlign: "justify" }}>{question}</p>
+                                <Form size="small" onSubmit={onSubmit}>
+                                    <Form.Field>
+                                        <textarea 
+                                            name="answer"
+                                            placeholder="Answer here ..."
+                                            value={answer}
+                                            onChange={onHandleChange} 
+                                            rows="5" 
+                                            col="5" 
+                                        />
+                                    </Form.Field>
+                                    <Form.Field>
+                                        <Form.Button color="red" size="tiny" content="Submit" />
+                                    </Form.Field>
+                                </Form>
+                            </>
                             )
                         }
                     </Segment>
@@ -42,11 +87,13 @@ const Scenario = ({ loadQuizScenario, quiz: { scenarios, setLoading } }) => {
 
 Scenario.propTypes = {
     loadQuizScenario: PropTypes.func.isRequired,
+    quizResult: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
     quiz: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    quiz: state.quiz
+    auth: state.auth
 });
 
-export default connect(mapStateToProps, { loadQuizScenario })(Scenario);
+export default connect(mapStateToProps, { loadQuizScenario, quizResult })(withRouter(Scenario));
