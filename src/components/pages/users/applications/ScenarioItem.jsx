52a9f85cce6ce543b7117f997ca0d1174c5722c2
Agung Maulana <iamaul@hotmail.com>
2020-05-17 07:33:37 +0700

    updated latest changes

--- src/components/pages/users/applications/ScenarioItem.jsx
+++ src/components/pages/users/applications/ScenarioItem.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {
     Form,
@@ -62,4 +61,4 @@ ScenarioItem.propTypes = {
     quizResult: PropTypes.func.isRequired
 }
 
-export default connect(null, { quizResult })(withRouter(ScenarioItem));
+export default connect(null, { quizResult })(ScenarioItem);
