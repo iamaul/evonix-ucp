38d63daffccd87fa28501adfb4e040406b09677a
Agung Maulana <iamaul@hotmail.com>
2020-05-19 15:36:04 +0700

    updated latest changes

--- src/components/pages/users/applications/Submitted.jsx
+++ src/components/pages/users/applications/Submitted.jsx
@@ -5,21 +5,23 @@ import {
     Header,
     Segment,
     Image,
-    Button
+    Button,
+    Divider
 } from 'semantic-ui-react';
 
 const Submitted = () => {
     return (
         <>
             <Grid textAlign="center" style={{ height: "60vh" }} verticalAlign="middle">
-                <Grid.Column style={{ maxWidth: 500 }}>
+                <Grid.Column style={{ maxWidth: 600 }}>
                     <Header as="h2" textAlign="center">
                         <Image src="/assets/images/evonix-logo.png" size="massive" />
                     </Header>
                     <Segment color="red" stacked>
-                        <Image src="https://media.giphy.com/media/h5Ct5uxV5RfwY/giphy.gif" size="medium" bordered /><br/>
-                        Finally, you have successfully submitted your application.<br/>
-                        <Button content="Back to UCP" size="small" as={Link} to="/applications" />
+                        <Image src="https://media.giphy.com/media/h5Ct5uxV5RfwY/giphy.gif" centered bordered /><br/>
+                        Finally, you have successfully submitted your application.
+                        <Divider hidden />
+                        <Button content="Continue" size="small" as={Link} to="/dashboard" />
                     </Segment>
                 </Grid.Column>
             </Grid>
