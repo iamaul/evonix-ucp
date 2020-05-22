be324ed83cc2368f8c52dff633222ebd0f72f4cf
Agung Maulana <iamaul@hotmail.com>
2020-05-22 12:08:23 +0700

    updated latest changes

--- src/components/pages/users/Characters.jsx
+++ src/components/pages/users/Characters.jsx
@@ -1,39 +1,90 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import DataTable from 'react-data-table-component';
 import { 
     Button, 
-    Header, 
-    Icon, 
     Segment, 
     Grid, 
-    Form, 
-    Modal
+    Image
 } from 'semantic-ui-react';
 
 import Sidebar from '../../layouts/sidebar/Sidebar';
+import Loader from '../../layouts/loader/Loader';
 
-import { createCharacter } from '../../actions/character';
+import { getUserCharacters } from '../../actions/character';
 
-const Characters = ({ createCharacter }) => {
-    const [open, setOpen] = useState(false);
-    const [formData, setFormData] = useState({ firstname: '', lastname: '', gender: 'male' });
+const ExpandedData = ({ data }) => (
+    <Grid>
+        <Grid.Column width={4}>
+            <Image src={`/assets/images/skins/${data.skin_id}.png`} size="medium" />
+        </Grid.Column>
+        <Grid.Column width={9}>
+            <p style={{ textAlign: 'justify' }}>
+                <b>Gender</b>: {data.gender === 0 ? 'Male' : data.gender === 1 ? 'Female' : 'Other'}<br/>
+                <b>Date of Birth</b>: {data.birth_day}/{data.birth_month}/{data.birth_year}<br/>
+                <b>Exp</b>: {data.exp}<br/>
+                <b>Money</b>: {data.money}<br/>
+                <b>Bank</b>: {data.bank}<br/>
+                <b>Bank Saving</b>: {data.bank_saving}<br/>
+                <b>Paycheck</b>: {data.paycheck}<br/>
+                <b>Max Health</b>: {data.max_health}<br/>
+                <b>Health</b>: {data.health}<br/>
+                <b>Armour</b>: {data.armour}<br/>
+                <b>Phone Number</b>: {data.phone_number}<br/>
+                <Divider hidden />
+                You have spent {data.play_second} seconds, {data.play_minute} minutes, and {data.play_hour} hours in the server!
+            </p>
+        </Grid.Column>
+    </Grid>
+);
 
-    const { firstname, lastname, gender } = formData;
+const Characters = ({ getUserCharacters , character: { character, setLoading } }) => {
 
-    const onChange = c => {
-        setFormData({ ...formData, [c.target.name]: c.target.value });
-        console.log(c.target.name + " " + c.target.value);
-        console.log(formData);
-    }
+    useEffect(() => {
+        getUserCharacters();
+        // eslint-disable-next-line
+    }, []);
 
-    const onModalClose = () => setOpen(false);
+    const { count, rows } = character;
 
-    const onSubmit = e => {
-        e.preventDefault();
-        
-        createCharacter({ firstname, lastname, gender });
-    }
+    const actions = (
+        <div>
+            <Button
+                content="Create A New Character"
+                color="green"
+                size="small"
+                as={Link}
+                to="/characters/create"
+            /><br/>
+            <small>You have created {count} characters.</small>
+        </div>
+    );
+
+    const columns = useMemo(() => [
+        {
+            name: 'Name',
+            selector: 'name',
+            sortable: true,
+            style: {
+                fontWeight: 'bold'
+            }
+        },
+        {
+            name: 'Last Login',
+            selector: 'lastlogin',
+            sortable: true,
+            cell: row => <div>{row.lastlogin !== 0 ? (<Moment unix fromNow>{row.lastlogin}</Moment>) : 'Not logged in yet.'}</div>
+        },
+        {
+            name: 'Level',
+            selector: 'level',
+            sortable: true,
+            cell: row => <div>{row.level}</div>
+        }
+        // eslint-disable-next-line
+    ], []);
 
     return (
         <>
@@ -42,66 +93,19 @@ const Characters = ({ createCharacter }) => {
                     <Sidebar />
                     <Grid.Column stretched width={12}>
                         <Segment>
-                            <Segment placeholder textAlign="center">
-                                <Header icon>
-                                    <Icon name="search" />
-                                    No characters are listed for this account.
-                                </Header>
-                                <Modal 
-                                    size="tiny" 
-                                    trigger={<Button primary onClick={() => setOpen(true)}>Add New</Button>}
-                                    open={open}
-                                    closeOnEscape={false}
-                                    closeOnDimmerClick={false} 
-                                    closeIcon
-                                    onClose={onModalClose}
-                                >
-                                    <Header icon="user plus" content="Create a new character" />
-                                    <Modal.Content>
-                                        <Form onSubmit={e => onSubmit(e)}>
-                                            <Form.Input
-                                                type="text"
-                                                name="firstname"
-                                                value={firstname}
-                                                placeholder="First Name"
-                                                onChange={c => onChange(c)}
-                                            />
-                                            <Form.Input
-                                                type="text"
-                                                name="lastname"
-                                                value={lastname}
-                                                placeholder="Last Name"
-                                                onChange={c => onChange(c)}
-                                            />
-                                            <Form.Group inline>
-                                                <label>Gender:</label>
-                                                <Form.Radio
-                                                    label="Male"
-                                                    name="gender"
-                                                    value="male"
-                                                    checked={gender === 'male'}
-                                                    onChange={c => onChange(c)}
-                                                />
-                                                <Form.Radio
-                                                    label="Female"
-                                                    name="gender"
-                                                    value="female"
-                                                    checked={gender === 'female'}
-                                                    onChange={c => onChange(c)}
-                                                />
-                                                <Form.Radio
-                                                    label="Other"
-                                                    name="gender"
-                                                    value="other"
-                                                    checked={gender === 'other'}
-                                                    onChange={c => onChange(c)}
-                                                />
-                                            </Form.Group>
-                                            <Form.Button color="red" content="Create" />
-                                        </Form>
-                                    </Modal.Content>
-                                </Modal>
-                            </Segment>
+                            {rows !== null && !setLoading ? (
+                                <DataTable
+                                    title="Characters"
+                                    columns={columns}
+                                    data={rows}
+                                    expandableRows
+                                    expandableRowsComponent={<ExpandedData />}
+                                    highlightOnHover
+                                    defaultSortField="lastlogin"
+                                />
+                            ) : (
+                                <Loader isLoading={setLoading} />
+                            )}
                         </Segment>
                     </Grid.Column>
                 </Grid>
@@ -111,7 +115,12 @@ const Characters = ({ createCharacter }) => {
 }
 
 Characters.propTypes = {
-    createCharacter: PropTypes.func.isRequired
+    getUserCharacters: PropTypes.func.isRequired
 }
 
-export default connect(null, { createCharacter })(Characters);
+const mapStateToProps = state => ({
+    auth: state.auth,
+    character: state.character
+});
+
+export default connect(mapStateToProps, { getUserCharacters })(Characters);
