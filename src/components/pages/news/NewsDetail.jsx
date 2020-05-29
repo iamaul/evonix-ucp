f77fec2ef28958b2a693a0e004851127a93ecd54
Agung Maulana <iamaul@hotmail.com>
2020-05-29 11:42:28 +0700

    updated latest changes

--- src/components/pages/news/NewsDetail.jsx
+++ src/components/pages/news/NewsDetail.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Moment from 'react-moment';
 import parse from 'html-react-parser';
-import { Container, Image } from 'semantic-ui-react';
+import { Container, Image, Divider } from 'semantic-ui-react';
 
 import Loader from '../../layouts/loader/Loader';
+
+import { getNewsDetail } from '../../actions/news';
 
 const NewsDetail = ({ getNewsDetail, news_detail: { news_detail, setLoading }, match }) => {
     useEffect(() => {
