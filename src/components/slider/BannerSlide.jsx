8b34625a983da6feb9b5bb162a9b6467d29d8d0f
Agung Maulana <iamaul@hotmail.com>
2020-06-01 18:39:14 +0700

    updated latest changes

--- src/components/slider/BannerSlide.jsx
+++ src/components/slider/BannerSlide.jsx
@@ -25,11 +25,11 @@ const BannerSlide = () => {
     return (
         <>
             <Swiper {...params}>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_1.png" rounded /></span></div>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_2.png" rounded /></span></div>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_3.png" rounded /></span></div>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_4.png" rounded /></span></div>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_5.png" rounded /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_1.png" size="massive" rounded /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_2.png" size="massive" rounded /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_3.png" size="massive" rounded /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_4.png" size="massive" rounded /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_5.png" size="massive" rounded /></span></div>
             </Swiper>
         </>
     )
