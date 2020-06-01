dec3bc645773d670c66bd335c2a234845fdfd1ab
Agung Maulana <iamaul@hotmail.com>
2020-06-01 13:48:22 +0700

    updated latest changes

--- src/components/slider/BannerSlide.jsx
+++ src/components/slider/BannerSlide.jsx
@@ -4,7 +4,6 @@ import { Image } from 'semantic-ui-react';
 
 const BannerSlide = () => {
     const params = {
-        spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
             delay: 2500,
@@ -24,14 +23,11 @@ const BannerSlide = () => {
     return (
         <>
             <Swiper {...params}>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_1.png" fluid /></span></div>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_2.png" fluid /></span></div>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_3.png" fluid /></span></div>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_4.png" fluid /></span></div>
-                <div className="swiper-slide"><span><Image src="/assets/images/banner_5.png" fluid /></span></div>
-                <div className="swiper-button-prev"></div>
-                <div className="swiper-button-next"></div>
-                <div className="swiper-pagination"></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_1.png" circular /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_2.png" circular /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_3.png" circular /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_4.png" circular /></span></div>
+                <div className="swiper-slide"><span><Image src="/assets/images/banner_5.png" circular /></span></div>
             </Swiper>
         </>
     )
