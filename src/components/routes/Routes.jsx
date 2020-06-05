b08a0b0587dffa1122b1df6884da508079df06ea
Agung Maulana <iamaul@hotmail.com>
2020-06-05 13:07:33 +0700

    updated latest changes

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -8,10 +8,6 @@ import NewsFeed from '../pages/news/NewsFeed';
 import NewsDetail from '../pages/news/NewsDetail';
 import Dashboard from '../pages/users/Dashboard';
 import Characters from '../pages/users/Characters';
-import CharacterAdminWarn from '../pages/users/admin_warn/CharacterAdminWarn';
-import CharacterInventory from '../pages/users/inventory/CharacterInventory';
-import CharacterVehicle from '../pages/users/vehicle/CharacterVehicle';
-import CharacterProperty from '../pages/users/property/CharacterProperty';
 import Settings from '../pages/users/Settings';
 
 import PrivateRoute from './PrivateRoute';
@@ -26,10 +22,6 @@ const Routes = () => {
             {/* <Route path="/blog" component={Blog} /> */}
             <PrivateRoute path="/dashboard" component={Dashboard} />
             <PrivateRoute path="/characters" component={Characters} />
-            <PrivateRoute path="/characters/:name/admin_records/:id" component={CharacterAdminWarn} />
-            <PrivateRoute path="/characters/:name/inventory/:id" component={CharacterInventory} />
-            <PrivateRoute path="/characters/:name/vehicle/:id" component={CharacterVehicle} />
-            <PrivateRoute path="/characters/:name/property/:id" component={CharacterProperty} />
             <PrivateRoute path="/settings" component={Settings} />
         </Switch>
     )
