871844755e83fa09ccd35400e7b5529bc52cf4f4
Agung Maulana <iamaul@hotmail.com>
2020-06-16 20:58:44 +0700

    fixed the closure

--- src/components/pages/auth/ResetPassword.jsx
+++ src/components/pages/auth/ResetPassword.jsx
@@ -62,49 +62,19 @@ const ResetPassword = ({
         }
     }
 
-    return (
-        <>
-            <Grid textAlign="center" style={{ height: '80vh' }} verticalAlign="middle">
-                <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" textAlign="center">
-                        <Image as={Link} src="/assets/images/evonix-logo.png" size="massive" to="/" />
-                    </Header>
-                    <Segment color="red" stacked>
-                        {verify_reset_new_password === null ? (<>
+    if (verify_reset_new_password === null) {
+        return (
+            <>
+                <Grid textAlign="center" style={{ height: '80vh' }} verticalAlign="middle">
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as="h2" textAlign="center">
+                            <Image as={Link} src="/assets/images/evonix-logo.png" size="massive" to="/" />
+                        </Header>
+                        <Segment color="red" stacked>
                             <Image src="https://media.giphy.com/media/kC3Z2WuxtXO8DyCUzG/giphy.gif" centered size="medium" /><br/>
                             The page link is invalid or session has been expired.
-                        </>) : (
-                                <Form size="large" onSubmit={onSubmit}>
-                                    <Form.Input 
-                                        type="password"
-                                        name="password" 
-                                        value={password}
-                                        icon="lock" 
-                                        iconPosition="left" 
-                                        placeholder="New Password"
-                                        onChange={onPasswordChange}
-                                        fluid 
-                                    />
-                                    <Form.Input 
-                                        type="password"
-                                        name="confirm_password" 
-                                        value={confirm_password}
-                                        icon="lock" 
-                                        iconPosition="left" 
-                                        placeholder="Confirm New Password"
-                                        onChange={onConfirmPasswordChange}
-                                        fluid 
-                                    />
-                                    <ReCAPTCHA
-                                        ref={recaptchaRef}
-                                        sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
-                                    /><br/>
-                                    <Form.Button color="red" fluid size="large" content="Reset" loading={requestResetNewPassword} />
-                                </Form>
-                            )
-                        }
-                    </Segment>
-                    <Divider hidden />
+                        </Segment>
+                        <Divider hidden />
                         <Button
                             info
                             as={Link}
@@ -112,6 +82,81 @@ const ResetPassword = ({
                         >
                             <Icon name="arrow alternate circle left"/>Back
                         </Button>
+                    </Grid.Column>
+                </Grid>
+                <footer>
+                    <p>
+                        <a href="http://facebook.com/EvonixRoleplay" target="_blank" rel="noopener noreferrer">
+                            <Icon name="facebook official" />
+                        </a>
+                        <a href="http://twitter.com/EvonixRoleplay" target="_blank" rel="noopener noreferrer">
+                            <Icon name="twitter square" />               
+                        </a> 
+                        <a href="http://instagram.com/evonixroleplay" target="_blank" rel="noopener noreferrer">
+                            <Icon name="instagram" />
+                        </a>               
+                        <br/>          
+                        <Icon name="copyright outline" /> 2020 EvoniX UCP.
+                    </p>
+                </footer>
+                <style>{`
+                    footer {
+                        position: fixed;
+                        left: 0;
+                        bottom: 0;
+                        width: 100%;
+                        text-align: center;
+                        padding: 10px;
+                    }
+                `}</style>
+            </>
+        )
+    }
+
+    return (
+        <>
+            <Grid textAlign="center" style={{ height: '80vh' }} verticalAlign="middle">
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as="h2" textAlign="center">
+                        <Image as={Link} src="/assets/images/evonix-logo.png" size="massive" to="/" />
+                    </Header>
+                    <Form size="large" onSubmit={onSubmit}>
+                        <Segment color="red" stacked>
+                            <Form.Input 
+                                type="password"
+                                name="password" 
+                                value={password}
+                                icon="lock" 
+                                iconPosition="left" 
+                                placeholder="New Password"
+                                onChange={onPasswordChange}
+                                fluid 
+                            />
+                            <Form.Input 
+                                type="password"
+                                name="confirm_password" 
+                                value={confirm_password}
+                                icon="lock" 
+                                iconPosition="left" 
+                                placeholder="Confirm New Password"
+                                onChange={onConfirmPasswordChange}
+                                fluid 
+                            />
+                            <ReCAPTCHA
+                                ref={recaptchaRef}
+                                sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                            /><br/>
+                            <Form.Button color="red" fluid size="large" content="Reset" loading={requestResetNewPassword} />
+                        </Segment>
+                    </Form>
+                    <Divider hidden />
+                    <Button
+                        info
+                        as={Link}
+                        to="/login"
+                    >
+                        <Icon name="arrow alternate circle left"/>Back
+                    </Button>
                 </Grid.Column>
             </Grid>
             <footer>
