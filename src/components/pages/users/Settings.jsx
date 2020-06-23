ba259dde06d54940ccd2436fe559290467b9c3e1
Agung Maulana <iamaul@hotmail.com>
2020-06-23 12:13:14 +0700

    fixed undefined Redirect

--- src/components/pages/users/Settings.jsx
+++ src/components/pages/users/Settings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { Header, Icon, Segment, Grid, Message, Button } from 'semantic-ui-react';
 
 import Sidebar from '../../layouts/sidebar/Sidebar';
