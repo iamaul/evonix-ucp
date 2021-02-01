b34088886cb4cb058f34ed7f4073802f0f679407
Agung Maulana <iamaul@hotmail.com>
2021-02-01 09:47:52 +0700

    updated: SA-MP API path url

--- src/components/actions/samp.js
+++ src/components/actions/samp.js
@@ -3,7 +3,7 @@ import { GET_API_SAMP_SERVER, GET_API_SAMP_SERVER_ERROR } from './types';
 
 export const getApiSampServer = () => async dispatch => {
     try {
-        const res = await axios.get('https://api.samp-servers.net/v2/server/167.99.65.76:7777');
+        const res = await axios.get('https://api.open.mp/server/167.99.65.76:7777');
         dispatch({ type: GET_API_SAMP_SERVER, payload: res.data });
     } catch (error) {
         dispatch({ type: GET_API_SAMP_SERVER_ERROR });
