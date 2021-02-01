a78114d1c01bd15d616f110c6ef566230cc105aa
Agung Maulana <iamaul@hotmail.com>
2021-02-01 09:51:57 +0700

    fixed: CORS issue

--- src/components/actions/samp.js
+++ src/components/actions/samp.js
@@ -3,7 +3,11 @@ import { GET_API_SAMP_SERVER, GET_API_SAMP_SERVER_ERROR } from './types';
 
 export const getApiSampServer = () => async dispatch => {
     try {
-        const res = await axios.get('https://api.open.mp/server/167.99.65.76:7777');
+        const res = await axios.get('https://api.open.mp/server/167.99.65.76:7777', {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
         dispatch({ type: GET_API_SAMP_SERVER, payload: res.data });
     } catch (error) {
         dispatch({ type: GET_API_SAMP_SERVER_ERROR });
