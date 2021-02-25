ebaf4ca59b7c9ac111b9dbd9a8c83b86d2d3d308
Agung Maulana <iamaul@hotmail.com>
2021-02-25 16:34:11 +0700

    updated samp

--- src/components/actions/samp.js
+++ src/components/actions/samp.js
@@ -3,11 +3,8 @@ import { GET_API_SAMP_SERVER, GET_API_SAMP_SERVER_ERROR } from './types';
 
 export const getApiSampServer = () => async dispatch => {
     try {
-        const res = await axios.get('https://api.open.mp/server/13.212.169.37:7777', {
-            headers: {
-                'Access-Control-Allow-Origin': 'https://ucp.evonix-rp.com'
-            }
-        });
+        const res = await axios.get('https://api.open.mp/server/13.212.169.37:7777');
+        console.log(res);
         dispatch({ type: GET_API_SAMP_SERVER, payload: res.data });
     } catch (error) {
         dispatch({ type: GET_API_SAMP_SERVER_ERROR });
