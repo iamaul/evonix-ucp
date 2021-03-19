c02ba53e3dc60c8ca63138921514f6ffe9aaa92c
Agung Maulana <iamaul@hotmail.com>
2021-03-20 02:12:06 +0700

    updated latest changes

--- src/components/actions/samp.js
+++ src/components/actions/samp.js
@@ -3,7 +3,7 @@ import api from '../utils/api';
 
 export const getApiSampServer = () => async dispatch => {
     try {
-        const res = await api.get('/api/v1/auth');
+        const res = await api.get('/api/v1/server');
         console.log(res);
         dispatch({ type: GET_API_SAMP_SERVER, payload: res.data });
     } catch (error) {
